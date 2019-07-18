0ac38ed9170dab3de270c82ba323e2ec9047bbab
Adrian Mostowski <adaxelx@o2.pl>
2019-07-18 12:23:43 +0200

    add animation to articles

--- src/templates/Articles.js
+++ src/templates/Articles.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { TimelineMax } from 'gsap/TweenMax';
+
 import Article from '../components/Article';
 
 const StyledContainer = styled.section`
@@ -7,12 +9,26 @@ const StyledContainer = styled.section`
   top: 180px;
   height: 50vh;
   width: 100%;
+  opacity: 0;
 `;
 
 const Articles = () => {
+  const con = React.createRef();
+
+  const animate = () => {
+    const cn = con.current;
+    const tl = new TimelineMax();
+
+    tl.addLabel('hide')
+      .to(cn, 0.1, { y: '50px' })
+      .addLabel('show')
+      .to(cn, 1, { y: '0px', opacity: 1 });
+  };
+
   return (
-    <StyledContainer>
+    <StyledContainer ref={con}>
       <Article />
+      {setTimeout(() => animate(), 3000)}
     </StyledContainer>
   );
 };
