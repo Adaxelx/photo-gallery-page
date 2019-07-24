37198962fa9086788df64e4408fd7ab0436065da
Adrian Mostowski <adaxelx@o2.pl>
2019-07-24 23:26:48 +0200

    Updates

---
+++ precache-manifest.3841deb826a06501681a2ec1c641e08b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e67f459cc2fd415c98ca37b81016b35",
+    "url": "/example-website/index.html"
+  },
+  {
+    "revision": "d0088567ba6e95bc1475",
+    "url": "/example-website/static/js/2.27584342.chunk.js"
+  },
+  {
+    "revision": "5465f1598afe04817249",
+    "url": "/example-website/static/js/main.f9172cc3.chunk.js"
+  },
+  {
+    "revision": "0fc3ee3117afe6eef241",
+    "url": "/example-website/static/js/runtime~main.97ae9a98.js"
+  },
+  {
+    "revision": "ef3b9d53e8e4d00e81c6ad609c9039d7",
+    "url": "/example-website/static/media/fontanna.ef3b9d53.jpg"
+  }
+]);
